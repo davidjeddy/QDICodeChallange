2086ac586e637a3419800943b9fa2ff788201a38
David J Eddy <me@davidjeddy.com>
2013-10-08 09:18:18 -0400

    Entry point added in app root.
    Will also contain application DocBlock

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Simple Contact Manager for QDI code Sample Example
+* I did want to make this in Python but to show my 
+* quality level and due to time constrants I opted
+* to go with a PHP.MySQL solution.
+*
+*@author David Eddy
+*@date 2013-10-08
+*@version 0.0.1
+*/
+
+// Redirect to the view
+require_once("./views/index.php");
