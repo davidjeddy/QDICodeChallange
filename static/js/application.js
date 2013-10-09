435a931e8e356b86df2de763fe21f1c1c7eaddf3
David J Eddy <me@davidjeddy.com>
2013-10-09 15:07:55 -0400

    Was able to refactor form.element -> json object assignments.
    appended data now sets id to the id, not fname.
    When a button is pressed the HTTP request method is determined from the *_data_button class.

--- static/js/application.js
+++ static/js/application.js
@@ -38,13 +38,10 @@ function crudData(action, data, httpType) {
 	/* Add action and http type to data package */
 	out_data = new Object();
 
-	//TODO Refactor this as a loop that is not dependant on a static form element list
 	if (data != null) {
-		out_data["fname"] = data[0].value;
-		out_data["lname"] = data[1].value;
-		out_data["city"]  = data[2].value;
-		out_data["state"] = data[3].value;
-		out_data["zip"]   = data[4].value;
+		$.each(data, function(i){
+			out_data[data[i].name] = data[i].value;
+		});
 	}
 
 	/* add the AJAX/REST actions */
@@ -73,24 +70,21 @@ function crudData(action, data, httpType) {
 			//TODO add this starter when w new letter is reached: '<div class="bs-example id="//first letter//">
 			new_html += '\
 			<div class="bs-example">\
-                <form action="patch">\
-                    <input type="hidden"    class="form-control" name="fname"    value="'+data[i].id+'" />\
-                    <input type="text"      class="form-control" name="fname"    value="'+data[i].fname+'" />\
-                    <input type="text"      class="form-control" name="lname"    value="'+data[i].lname+'" />\
-                    <input type="text"      class="form-control" name="city"     value="'+data[i].city+'" />\
-                    <input type="text"      class="form-control" name="state"    value="'+data[i].state+'" />\
-                    <input type="text"      class="form-control" name="zip"      value="'+data[i].zip+'" />\
+                <form>\
+                    <input type="hidden"    class="form-control" name="id" 		value="'+data[i].id+'" />\
+                    <input type="text"      class="form-control" name="fname" 	value="'+data[i].fname+'" />\
+                    <input type="text"      class="form-control" name="lname" 	value="'+data[i].lname+'" />\
+                    <input type="text"      class="form-control" name="city" 	value="'+data[i].city+'" />\
+                    <input type="text"      class="form-control" name="state" 	value="'+data[i].state+'" />\
+                    <input type="text"      class="form-control" name="zip" 	value="'+data[i].zip+'" />\
                     <button type="button"   class="btn btn-success update_data_button disabled" data-loading-text="Update" >Update</button>\
                     <button type="button" 	class="btn btn-warning delete_data_button disabled" data-loading-text="Delete" >Delete</button>\
                 </form>\
             </div>';
-
-			console.log(data);
 		});
 
-	$("#data_container.scrollspy").append(new_html);
-
-	//TODO re-init all the buttons that just got added
+		//Append all the new data
+		$("#data_container.scrollspy").append(new_html);
 
 
 
@@ -131,15 +125,25 @@ $( document ).on( "blur", "form input.form-control, form button.btn", function()
 
 
 /* The action button (Save/Add) */
-$( document ).on( "form button.btn", function() {
-	
-	return crudData(current_form_elem.attr('action'), current_form_elem.serializeArray());
+$( document ).on( "click", "form button.btn", function() {
+
+	//Determine HTTP request method based on the class the button contains
+	var http_method = "get"; //default for reading
+
+	if ($(this).hasClass("create_data_button")) {
+		http_method = "post";
+	//'Read' would be here but is set by default
+	} else if ($(this).hasClass("update_data_button")) {
+		http_method = "patch";
+	} else if ($(this).hasClass("delete_data_button")) {
+		http_method = "delete";
+	}
+
+	return crudData(http_method, current_form_elem.serializeArray());
 });
 
 
 
 $( document ).ready(function() {
     var data = crudData();
-
-
 });
