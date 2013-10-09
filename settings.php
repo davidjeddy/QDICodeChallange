74e4a6aef2937ddecb3eb4d21c2d2dcd8b1e340d
David J Eddy <me@davidjeddy.com>
2013-10-08 21:50:00 -0400

    Moved README to docs dir.
    added db conn CONST to settings.php

--- settings.php
+++ settings.php
@@ -6,3 +6,7 @@
 // do not include trailing slash
 define("file_root", "C:\Source\WebApps1\web\devel\qdi");
 define("dir_sep", "\\");
+
+define("db_user", "Pheagey");
+define("db_pass", "Asdf1234");
+define("db_table", "qdi");
