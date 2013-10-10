b9ee016f950e5166b344f19ecf3b45d229dc52cb
David J Eddy <me@davidjeddy.com>
2013-10-09 21:51:38 -0400

    Removed unused CONST from settings

--- settings.php
+++ settings.php
@@ -3,10 +3,6 @@
 *Application settings
 */
 
-// do not include trailing slash
-define("file_root", "C:\Source\WebApps1\web\devel\qdi");
-define("dir_sep", "\\");
-
 define("db_user", "root");
 define("db_pass", "");
 define("db_host", "localhost");
