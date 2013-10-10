d97151d5248c4eaff45cbe22516c04eee973ffbd
David J Eddy <me@davidjeddy.com>
2013-10-09 20:32:26 -0400

    Updated table creation SQL

--- docs/contact_sql.sql
+++ docs/contact_sql.sql
@@ -7,14 +7,20 @@
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";# MySQL returned an empty result set (i.e. zero rows).
+
+SET time_zone = "+00:00";# MySQL returned an empty result set (i.e. zero rows).
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;# MySQL returned an empty result set (i.e. zero rows).
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;# MySQL returned an empty result set (i.e. zero rows).
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;# MySQL returned an empty result set (i.e. zero rows).
+
+/*!40101 SET NAMES utf8 */;# MySQL returned an empty result set (i.e. zero rows).
+
 
 --
 -- Table structure for table `contacts`
@@ -28,10 +34,14 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   `state` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
   `zip` int(5) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Contain contact and location data' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Contain contact and location data' AUTO_INCREMENT=1 ;# MySQL returned an empty result set (i.e. zero rows).
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
-ALTER TABLE `test`.`contacts` ADD COLUMN `deleted` DATETIME NULL DEFAULT NULL  AFTER `zip` ;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;# MySQL returned an empty result set (i.e. zero rows).
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;# MySQL returned an empty result set (i.e. zero rows).
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;# MySQL returned an empty result set (i.e. zero rows).
+
+
+ALTER TABLE `contacts` ADD COLUMN `deleted` DATETIME NULL DEFAULT NULL  AFTER `zip` ;# MySQL returned an empty result set (i.e. zero rows).
