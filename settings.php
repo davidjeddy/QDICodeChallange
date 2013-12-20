2fe0735ebcbe8203b52c343673b28995aae151d3
David J Eddy <me@davidjeddy.com>
2013-12-20 13:05:47 -0500

    Decided to leave default settings in place. End users can edit as needed.

--- settings.php
+++ settings.php
@@ -3,8 +3,8 @@
 *Application settings
 */
 
-define("db_user", "");
+define("db_user", "root");
 define("db_pass", "");
-define("db_host", "");
-define("db_name", "");
-define("db_table", "");
+define("db_host", "localhost");
+define("db_name", "conman");
+define("db_table", "contacts");
